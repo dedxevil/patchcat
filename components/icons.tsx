74dba795d305f17083813624dad02d6020a0f212
Subash Chandran <arajagam@gmail.com>
2025-09-25 12:28:09 +0530

    reposnsives fix

--- components/icons.tsx
+++ components/icons.tsx
@@ -36,10 +36,12 @@ export const PlusIcon: React.FC<IconProps> = (props) => (
 );
 
 export const HistoryIcon: React.FC<IconProps> = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
-    <path d="M1 4v6h6" />
-    <path d="M3.51 15a9 9 0 1 0 2.13-9.36L1 10" />
-  </svg>
+  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"  {...props}><path d="M13 3c-4.97 0-9 4.03-9 9H1l3.89 3.89.07.14L9 12H6c0-3.87 3.13-7 7-7s7 3.13 7 7-3.13 7-7 7c-1.93 0-3.68-.79-4.94-2.06l-1.42 1.42C8.27 19.99 10.51 21 13 21c4.97 0 9-4.03 9-9s-4.03-9-9-9zm-1 5v5l4.28 2.54.72-1.21-3.5-2.08V8H12z"/></svg>
+
+  // <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
+  //   <path d="M1 4v6h6" />
+  //   <path d="M3.51 15a9 9 0 1 0 2.13-9.36L1 10" />
+  // </svg>
 );
 
 export const SettingsIcon: React.FC<IconProps> = (props) => (
